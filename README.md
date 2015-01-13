06e18fb07cf57e387c280581d1a96d27bcdd95ea
Tanny O'Haley <tannyo@gmail.com>
2015-01-13 10:29:03 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # load.js
-Script loader
+
+My version of a script loader.
+
+## Usage
+
+<script src="path/to/load.js"></script>
+
+Edit the array of script location objects in load.js. Include load.js at the bottom of your html.
+
+## Settings
+
+**URL Object**
+```
+{
+  src: string,
+  wait: boolean,
+  add: boolean
+}
+```
+
+### src
+String. URL of script file.
+
+### wait
+Boolean. true, wait for script to load before loading other scripts. false, continue loading script files.
+
+### add
+Boolean. true, load script file. false, skip loading script file.
+
+## Example
+Wait until jQuery is loaded from a CDN, then load the rest of the files asynchronously. Don't add video.ga.js unless there is a video to play. Don't add downloads.js unless the user is on the downloads page.
+
+```
+scripts = [
+  {src: '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', wait: true, add: true},
+  {src: '/js/utils.js?201410081725', wait: false, add: true},
+  {src: '/js/video.ga.js?201410081725', wait: false, add: document.getElementById("video-blocker")},
+  {src: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js', wait: false, add: true},
+  {src: '/js/downloads.js?201412031612', wait: false, add: /^downloads/.test(document.body.className)}
+];
+```
+
+## Issues
+
+Have a bug? Please create an [issue](https://github.com/tannyo/load.js/issues) here on GitHub!
+
+## Contributing
+
+Want to contribute? Great! Just fork the project, make your changes and open a [pull request](https://github.com/tannyo/load.js/pulls).
+
+## Changelog
+* v0.10 2014-07-01 TKO Created by Tanny O'Haley.
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 [Tanny O'Haley](http://tanny.ica.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ load.js
@@ -0,0 +1,108 @@
+/*
+ * load.js
+ * A simple script loader.
+ *
+ * Edit the scripts array of objects and add this to the bottom of your html page.
+ * Documentation and license information at https://github.com/tannyo/load.js.
+ */
+
+/*jslint browser: true, devel: true, plusplus: true, indent: 2 */
+/*global debug */
+(function loadScripts() {
+  'use strict';
+
+  var i = 0,
+    scripts = [
+      {src: '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', wait: true, add: true},
+      {src: '/js/utils.js?201410081725', wait: false, add: true},
+      {src: '/js/video.ga.js?201410081725', wait: false, add: document.getElementById("video-blocker")},
+      {src: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js', wait: false, add: true},
+      {src: '/js/downloads.js?201412031612', wait: false, add: /^downloads/.test(document.body.className)}
+    ];
+
+  function addScript() {
+    var script;
+
+    function scriptLoaded() {
+      i++;
+      addScript();
+    }
+
+    if (i >= scripts.length) {
+      return;
+    }
+
+    // Should we skip this script?
+    if (!scripts[i].add) {
+      i++;
+      addScript();
+      return;
+    }
+
+    // Create a new script element.
+    script = document.createElement('script');
+
+    // Should we wait to load the next script after this script has loaded?
+    if (scripts[i].wait) {
+      script.onload = scriptLoaded;
+
+      // Support script load errors in IE < 9, if you don't need this support,
+      // delete this section.
+      if (window.attachEvent && !window.addEventListener) {
+        // Support errors in readyState. http://stackoverflow.com/a/18840568
+        script.onreadystatechange = function () {
+          if (script.readyState === "complete") {
+            // Add the script to the page to run the script.
+            document.body.appendChild(script);
+            script.onreadystatechange = null;
+            scriptLoaded();
+            return;
+          }
+
+          var firstState = script.readyState;
+
+          // hack: calling 'children' property changes node's readyState from
+          // 'loaded' to complete (if script was loaded normally) or to 'loading'
+          // - if error detected. This will throw an error if the script loaded
+          // successfully and not if there is an error.
+          try {
+            script.children;
+          } catch (ignore) {}
+          if (firstState === "loaded" && script.readyState === "loading") {
+            // Error loading required script. Usually an error function would be
+            // called, but since we are not detecting errors, no error function
+            // is called.
+            script.onreadystatechange = null;
+          }
+        };
+      }
+
+      // Add the script src.
+      script.src = scripts[i].src;
+      // Wait until the script has loaded before adding it to the DOM.
+    } else {
+      // Add the script src.
+      script.src = scripts[i].src;
+
+      // Append the script to the body to allow multiple scripts to load at the
+      // same time.
+      document.body.appendChild(script);
+
+    }
+
+    if (debug) {
+      console.log("loading: %c%s %c%s", "color:blue", script.src, "color:darkred", (scripts[i].wait ? "synchronous" : "asynchronous"));
+    }
+
+    // Should we load the next script now?
+    if (scripts[i].wait) {
+      return;
+    }
+
+    i++;
+    addScript();
+  }
+
+  // Start loading scripts.
+  addScript();
+}());
